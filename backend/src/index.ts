e84d82b75a5850abbb3d8f6f9896a46aa52a6970
Bharat Maheshwari <119489267+bharatm29@users.noreply.github.com>
2024-02-06 10:57:30 +0530

    init backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,25 @@
-const express = require('express');
+import express from "express";
+import bodyParser from "body-parser";
+import { timeTableSchema } from "./types";
+
+let jsonParser = bodyParser.json();
+
 const app = express();
 
+app.post("/timetable", jsonParser, function (req, res) {
+const validatedData = timeTableSchema.safeParse(req.body);
+  if (validatedData.success) {
+        console.log(validatedData.data);
+    const resObj = {
+      res: "hi",
+    };
+    res.json(resObj);
+  } else {
+    const resObj = {
+      res: "nono. Bad request",
+    };
+    res.json(resObj);
+  }
+});
 
+app.listen(4000);
