5be3ae230a11a65f3d6628d30813591bed631042
ArshhGill <arshh.gill@outlook.com>
2024-02-06 11:47:50 +0530

    stuff bkmb and me did

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,20 +6,26 @@ let jsonParser = bodyParser.json();
 
 const app = express();
 
-app.post("/timetable", jsonParser, function (req, res) {
-const validatedData = timeTableSchema.safeParse(req.body);
-  if (validatedData.success) {
-        console.log(validatedData.data);
-    const resObj = {
-      res: "hi",
-    };
-    res.json(resObj);
-  } else {
-    const resObj = {
-      res: "nono. Bad request",
-    };
-    res.json(resObj);
-  }
+app.get("/timetable", (req, res) => {
+    res.json({ "/timetable/{}": "course / date" });
 });
+
+app.post("/timetable", jsonParser, function (req, res) {
+    const validatedData = timeTableSchema.safeParse(req.body);
+    if (validatedData.success) {
+        console.log(validatedData.data);
+        const resObj = {
+            res: "hi",
+        };
+        res.json(resObj);
+    } else {
+        const resObj = {
+            res: "nono. Bad request",
+        };
+        res.json(resObj);
+    }
+});
+
+app.get("/timetable/");
 
 app.listen(4000);
--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,18 +1,18 @@
 import * as zod from "zod";
 
 export const timeTableSchema = zod.object({
-  courses: zod.array(
-    zod.object({
-      name: zod.string(),
-      instructor: zod.string(),
-      batch: zod.string(),
-      lectures: zod.array(
+    courses: zod.array(
         zod.object({
-          date: zod.string().refine((val) => val.length === 8),
-          room: zod.string(),
-          time: zod.string(),
-        }),
-      ),
-    }),
-  ),
+            name: zod.string(),
+            instructor: zod.string(),
+            batch: zod.string(),
+            lectures: zod.array(
+                zod.object({
+                    date: zod.string().refine((val) => val.length === 8),
+                    room: zod.string(),
+                    time: zod.string(),
+                })
+            ),
+        })
+    ),
 });
