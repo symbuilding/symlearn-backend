901dd102787cf3a469ff8c33b414702783c1ea29
bharatm29 <bharatmhes@gmail.com>
2024-02-08 20:53:29 +0530

    post and date-wise get working now :)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { timeTableSchema, classPatternSchema, Lecture, Course, lectureSchema } from "./types";
+import {
+    timeTableSchema,
+    classPatternSchema,
+    Course,
+    lectureSchema,
+    Lecture,
+} from "./types";
 import timetableData from "./data/timetable.json";
+import * as fs from "fs";
 
 const getTimetableData = async () => {
     return timetableData;
@@ -19,28 +26,30 @@ app.get(
     "/timetable/course-wise/:coures_name/:date",
     jsonParser,
     async function (req, res) {
-        console.log(req.params.coures_name, req.params.date);
+        const courseName = req.params.coures_name;
+        const lectureDate = req.params.date;
 
         const data = await getTimetableData();
 
         let desired_course: Course | undefined;
 
         data.courses.forEach((course) => {
-            if (
-                course.name.toLowerCase() ===
-                req.params.coures_name.toLowerCase()
-            ) {
+            if (course.name.toLowerCase() === courseName.toLowerCase()) {
                 desired_course = course;
             }
         });
 
         if (!desired_course) {
             return res.json({
-                error: `Provided course, ${req.params.coures_name} does not exist`,
+                error: `Provided course, ${courseName} does not exist`,
             });
         }
 
-        res.json(desired_course.lectures);
+        res.json(
+            desired_course.lectures.filter(
+                (lecture) => lecture.date === lectureDate
+            )
+        );
 
         /*
          *FIXME:  Do we need this :c
@@ -61,20 +70,54 @@ app.get(
     }
 );
 
-// TODO:
-// app.get("/timetable/date-wise/:date", (req, res) => {});
+app.get("/timetable/date-wise/:date", async (req, res) => {
+    const lectureDate = req.params.date;
 
-app.post("/timetable/:course_name", jsonParser, function (req, res) {
+    const data = await getTimetableData();
+
+    const lectures: Lecture[] = [];
+
+    data.courses.forEach((course) => {
+        course.lectures.forEach(lecture => {
+            if(lecture.date === lectureDate){
+                lectures.push(lecture);
+            }
+        })
+    });
+
+    res.json({ lectures });
+});
+
+app.post("/timetable/:course_name", jsonParser, async function (req, res) {
     const course_name = req.params.course_name;
     const validatedData = lectureSchema.safeParse(req.body);
-    if (validatedData.success){
+    if (validatedData.success) {
         const lectures = validatedData.data;
-        console.log(lectures);
-        res.json({
-            "body": "got it mate"
-        })
-    }
 
+        const data = await getTimetableData();
+
+        const desired_course = data.courses.find(
+            (course) => course.name.toLowerCase() === course_name.toLowerCase()
+        );
+
+        if (!desired_course) {
+            return res.status(405).json({
+                error: `Provided course, ${course_name} does not exist`,
+            });
+        }
+
+        lectures.lectures.forEach((lecture) => {
+            desired_course.lectures.push({ lid: 10, ...lecture });
+        });
+
+        fs.writeFileSync("src/data/timetable.json", JSON.stringify(data));
+
+        res.json("Lecture has been added");
+    } else {
+        res.status(405).json({
+            error: "Invalid body format for this endpoint.",
+        });
+    }
 });
 
 app.put("/timetable/:id", jsonParser, function (req, res) {
